e8a6327d3b48a5447d0f89682273692b96c35e1a
=Adrian <=s10258324@connect.np.edu.sg>
2025-05-07 14:30:29 +0800

    Updated index.php to trigger pipeline

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Sample PHP file
-$name = "Mr Adrian -CNAS";
-$message = "Hello, $name! Welcome to CNAS 1.";
+$name = "Mr Adrian - CNAS";
+$message = "Hello, $name! CNAS has been changed.";
 echo $message;
 ?>
